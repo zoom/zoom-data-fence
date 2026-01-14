3200fd9375fd8777f1145bc5ea809f30ff3c4840
Nagarjuna Pamu <nagarjuna.pamu@zoom.us>
2026-01-14 12:24:26 +0530

    Format PartitionedGrantStatements.

--- src/main/java/us/zoom/data/dfence/providers/snowflake/models/PartitionedGrantStatements.java
+++ src/main/java/us/zoom/data/dfence/providers/snowflake/models/PartitionedGrantStatements.java
@@ -3,7 +3,6 @@ package us.zoom.data.dfence.providers.snowflake.models;
 import java.util.List;
 
 public record PartitionedGrantStatements(
-        List<List<String>> ownershipStatements,
-        List<List<String>> nonOwnershipStatements) {
-}
+    List<List<String>> ownershipStatements,
+    List<List<String>> nonOwnershipStatements) {}
 
